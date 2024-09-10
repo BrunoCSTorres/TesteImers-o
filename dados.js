a31df77c3cb7c1359e7ebf25cd4dc8c994c5fa9d
Bruno Torres <92133023+BrunoCSTorres@users.noreply.github.com>
2024-09-09 23:51:57 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+  {
+    titulo: "Rayssa Leal",
+    descricao:
+      "Rayssa Leal, conhecida como Fadinha, é uma skatista profissional brasileira. Com apenas [idade], ela se tornou a mais jovem medalhista olímpica da história do Brasil. [Adicionar mais informações sobre suas conquistas]",
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags: "skate fada sk8",
+  },
+  {
+    titulo: "Beatriz Souza",
+    descricao:
+      "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024. Conhecida por sua técnica e força, ela se tornou um dos principais nomes do judô brasileiro. [Adicionar mais informações sobre suas conquistas]",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza", // Substituir por link da Wikipédia ou outro site relevante
+    tags: "judô judo judoca judoka",
+  },
+  {
+    titulo: "Rebeca Andrade",
+    descricao:
+      "Rebeca Andrade é uma ginasta artística brasileira que conquistou diversas medalhas em competições nacionais e internacionais, incluindo os Jogos Olímpicos. Conhecida por sua versatilidade e dificuldade nos exercícios, ela é considerada uma das maiores ginastas brasileiras de todos os tempos. [Adicionar mais informações sobre suas conquistas]",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags: "ginastica ginástica ouro",
+  },
+];
